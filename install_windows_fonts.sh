3f78d35590b14fcbaa99f6a1588e9593f157c3be
Phan Luan <luan.phan@edge-works.net>
2017-04-18 10:48:55 +0700

    vni fonts

--- install_windows_fonts.sh
+++ install_windows_fonts.sh
@@ -2,7 +2,13 @@
 sudo apt-get install -y fontconfig
 mkdir -p ~/.fonts
 cd ~/.fonts
+
 wget https://github.com/phanminhluan/direct-links/blob/master/windows_10_fonts.tar.gz?raw=true  -O windows_10_fonts.tar.gz
 tar -zxvf windows_10_fonts.tar.gz 
 rm windows_10_fonts.tar.gz
+
+wget https://github.com/phanminhluan/direct-links/blob/master/vni_fonts.tar.gz?raw=true  -O vni_fonts.tar.gz
+tar -zxvf vni_fonts.tar.gz
+rm vni_fonts.tar.gz
+
 fc-cache -f -v
