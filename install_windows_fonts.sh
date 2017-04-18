7f12b35c52ff09d68fd0d3cb7951fa1486d1163c
Phan Luan <luan.phan@edge-works.net>
2017-04-18 10:45:56 +0700

    install fontconfig

--- install_windows_fonts.sh
+++ install_windows_fonts.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt-get install -y fontconfig
 mkdir -p ~/.fonts
 cd ~/.fonts
 wget https://github.com/phanminhluan/direct-links/blob/master/windows_10_fonts.tar.gz?raw=true  -O windows_10_fonts.tar.gz
