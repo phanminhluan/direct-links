be73e95251b39a663c6d292ba27659221c428554
Phan Luan <luan.phan@edge-works.net>
2017-05-12 09:33:26 +0700

    update windows 10 fonts

--- install_windows_fonts.sh
+++ install_windows_fonts.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 sudo apt-get install -y fontconfig
-mkdir -p ~/.fonts
-cd ~/.fonts
 
-wget https://github.com/phanminhluan/direct-links/blob/master/windows_10_fonts.tar.gz?raw=true  -O windows_10_fonts.tar.gz
-tar -zxvf windows_10_fonts.tar.gz 
-rm windows_10_fonts.tar.gz
+wget https://github.com/phanminhluan/direct-links/blob/master/fonts.tar.gz?raw=true  -O fonts.tar.gz
+tar -zxvf fonts.tar.gz
+rm fonts.tar.gz
 
-wget https://github.com/phanminhluan/direct-links/blob/master/vni_fonts.tar.gz?raw=true  -O vni_fonts.tar.gz
-tar -zxvf vni_fonts.tar.gz
-rm vni_fonts.tar.gz
-
+sudo rm -rf /usr/share/fonts/ms_fonts
 sudo mkdir -p /usr/share/fonts/ms_fonts
 sudo mv *.ttf /usr/share/fonts/ms_fonts/
 
